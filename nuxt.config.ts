b2ecaa179a29c579a153b73ffb2002dd33d814fd
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-08-01 00:40:39 +0300

    add basic necessities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 import { defineNuxtConfig } from 'nuxt'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  target: 'static',
+  nitro: {
+    preset: isDev ? 'node-server' : 'aws-lambda'
+  },
+  buildModules: [
+    '@pinia/nuxt',
+  ],
+  modules: [
+    'nuxt-ionic',
+    '@nuxtjs/tailwindcss',
+    'nuxt-icons',
+  ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    i am a test page
+    <nuxt-icon name="fishing-hook" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wrapper {
+  @apply
+  flex
+  justify-center
+  align-middle
+  text-white
+  bg-blue-500
+  h-screen
+}
+</style>
