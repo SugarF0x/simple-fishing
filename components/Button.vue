60ef98e8079c6b02fcba24f65b9b945ce5b9b72d
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-08-07 16:55:20 +0300

    steal button from zalgo

---
+++ components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  @apply
+  inline-flex
+  items-center
+  py-2
+  px-4
+  border-white
+  border-2
+  rounded
+  bg-gray-300
+  bg-opacity-0
+  transition
+  hover:bg-opacity-20
+  active:bg-opacity-10
+  disabled:opacity-50
+  disabled:hover:bg-opacity-0
+  text-white
+  font-bold
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,23 @@
+<script setup lang="ts">
+import { NuxtIcon, Button } from '#components'
+</script>
+
 <template>
   <div class="wrapper">
     i am a test page
     <nuxt-icon name="fishing-hook" />
+
+    <Button> i am a button </Button>
   </div>
 </template>
 
 <style scoped lang="scss">
 .wrapper {
   @apply
+  p-8
   flex
+  flex-col
   justify-center
-  align-middle
-  text-white
-  bg-blue-500
-  h-screen
+  items-center
 }
 </style>
