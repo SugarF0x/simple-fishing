0e6f0e823eee661e1f3af073ddbcfe36333f5821
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-08-13 18:41:17 +0300

    send 401 instead of login redirect on no token

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,12 +1,15 @@
-import { useCookie, sendRedirect, defineMiddleware } from 'h3'
+import { useCookie, sendError, defineMiddleware, createError } from 'h3'
 
-export default defineMiddleware(async (event) => {
+export default defineMiddleware((event) => {
   if (event.req.originalUrl.split('/').slice(-1)[0].includes('.')) return
   if (event.req.originalUrl.includes('user')) return
 
   const token = useCookie(event, 'token')
   if (!token) {
-    return await sendRedirect(event, '/user/login')
+    sendError(event, createError({
+      statusCode: 401,
+      data: 'No authorization token found'
+    }))
   } else {
     event.context.auth = { user: 'dev-user-from-middleware' }
   }
