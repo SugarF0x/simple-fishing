997b553b6c2d6b5ed0a18948750803dee1304b0e
Alexey Tuzov <sugarfox.dev@gmail.com>
2022-08-13 19:27:48 +0300

    Revert "send 401 instead of login redirect on no token"
    This reverts commit 0e6f0e823eee661e1f3af073ddbcfe36333f5821.

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,15 +1,12 @@
-import { useCookie, sendError, defineMiddleware, createError } from 'h3'
+import { useCookie, sendRedirect, defineMiddleware } from 'h3'
 
-export default defineMiddleware((event) => {
+export default defineMiddleware(async (event) => {
   if (event.req.originalUrl.split('/').slice(-1)[0].includes('.')) return
   if (event.req.originalUrl.includes('user')) return
 
   const token = useCookie(event, 'token')
   if (!token) {
-    sendError(event, createError({
-      statusCode: 401,
-      data: 'No authorization token found'
-    }))
+    return await sendRedirect(event, '/user/login')
   } else {
     event.context.auth = { user: 'dev-user-from-middleware' }
   }
